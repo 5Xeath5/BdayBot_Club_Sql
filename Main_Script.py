0f5284d9a9449fa63d577223f44a1f8a8a9c10f9
Shannon Jay Narsico <shannonjaynarsico@berkeley.edu>
2023-07-20 15:12:25 -0700

    Intall required pip and set up python file

---
+++ Main_Script.py
@@ -0,0 +1,9 @@
+import os
+from sqlalchemy import create_engine, text
+
+engine = create_engine(os.environ["DATABASE_URL"])
+conn = engine.connect()
+
+res = conn.execute(text("SELECT now()")).fetchall()
+print(res)
+
